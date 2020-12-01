b2fb023c783b7e085b58fa035ecfd294506990a5
Mihailo Trisovic <mihailo.trisovic@gmail.com>
2020-12-01 04:27:58 -0800

    Izmenjen pocetni interfejs

--- code/include/World.hpp
+++ code/include/World.hpp
@@ -21,16 +21,18 @@ public:
 
     void show();
 
-    void main_menu();
 
 public slots:
     void start();
     void quit();
-
+    void main_menu();
+    void show_battles();
 protected:
     QGraphicsScene *scene = nullptr;
     QGraphicsView  *view  = nullptr;
-
+private:
+    int m_in_game=0;
+    int m_started=0;
 };
 
 #endif // WORLD_HPP
