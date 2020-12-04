956f49e918c3c842fa55113492ce84045767f872
Luka Miletic <lukamilecki998@gmail.com>
2020-12-04 23:46:45 +0100

    Implementirana kolizija rakete sa zidovima(ima bagova).

--- code/src/Rocket.cpp
+++ code/src/Rocket.cpp
@@ -27,9 +27,25 @@ Rocket::Rocket(float x, float y, float r, int rocket_power,Input* input,int id, 
 }
 void Rocket::move()
 {
+
+//    QList<QGraphicsItem> colliding_items = collidingItems();
+//    for (int i = 0; i < colliding_items.size(); i++)
+//        if (typeid(*(colliding_items[i])) == typeid(Wall))
+
     setPos(x() - m_pravac_x,y() - m_pravac_y);
-//    if (!scene()->collidingItems(this).isEmpty()) {
-//    }
+
+    if (!scene()->collidingItems(this).isEmpty()) {
+       //if (typeid(scene()->collidingItems(this).first()) == 0){
+            setPos(x() + m_pravac_x,y() + m_pravac_y);
+            auto new_x = m_pravac_x * cos(m_rotation) - m_pravac_y * sin(m_rotation);
+            auto new_y = m_pravac_x * sin(m_rotation) + m_pravac_y * cos(m_rotation);
+            m_pravac_x = new_x;
+            m_pravac_y = new_y;
+            //m_rotation = m_rotation;
+            setPos(x() - m_pravac_x,y() - m_pravac_y);
+        //}
+    }
+
 
     m_life_time++;
     if(m_life_time > 150)
